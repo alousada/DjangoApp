bf152a6f21328f21871783ccae95cd36e95cec66
Alvimar Alves Lousada <alvimar.lousada@lousada.com>
2022-06-11 14:01:07 -0300

    urls

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -144,3 +144,4 @@ class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     choices = models.ManyToManyField(Choice)
 
+#    Other fields and methods you would like to design
